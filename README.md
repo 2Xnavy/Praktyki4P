ea164d9cf0f5f647bdce9b4b293acdfb3fbc3e3d
2Xnavy <xnoisuu@gmail.com>
2022-11-08 01:07:14 +0100

    Aktualizacja brancha main
    Włożenie do maina ostatecznych plików

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Praktyki4P
-Obecnie main to to co ja zrobiłem w ramach mojego podejścia.
-@Weronika, możesz z tego korzystać jak chcesz.
-Jest to w main'ie bo chciałem sprawić żeby Pull Requesty były widoczne,
-więc oczywiście możesz wszystko usunąć z main'a i włożyć tu swoje pliki.
+Już nic więcej nie planujemy zmieniać.
---
+++ script.js
@@ -0,0 +1,18 @@
+function showAlert() {
+
+    let i = document.getElementById("imie").value;
+    let n = document.getElementById("nazwisko").value;
+    let w = document.getElementById("wiek").value;
+    let e = document.getElementById("email").value;
+    let g = document.getElementById("gender").value;
+    let s = document.querySelector('input[name="skinColor"]:checked').value;
+
+    let hasNumber = /\d/;
+
+    if (w >= 1 && w <= 100 && hasNumber.test(i) == false && hasNumber.test(n) == false && i != "" && n != "" && w != "" && e != "" && g == "Kobieta" || g == "Mężczyzna" && s == "white" || s == "yellow" || s == "brown" || s == "black" || s == "red") {
+        alert("FORMULARZ WYPEŁNIONY POPRAWNIE. ZGŁOSZENIE PRZYJĘTE.");
+    }
+    else {
+        alert("FORMULARZ WYPEŁNIONY NIEPOPRAWNIE. ZGŁOSZENIE ODRZUCONE.");
+    }
+}
