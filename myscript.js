c55f1fdc6487d79851f36edd6628f3c8e1b03341
2Xnavy <xnoisuu@gmail.com>
2022-11-28 19:12:46 +0100

    Skończenie strony 'searchingResults'

--- myscript.js
+++ myscript.js
@@ -16,3 +16,14 @@ function enableScrolling(){
     window.onscroll = function(){};
 }
 
+
+function showFilteredResults(){
+    let resultList = document.getElementsByClassName("resultList");
+    
+    for(let i = 0; i<18; i++){
+        let img = document.createElement("img");
+        img.src="images/troy.jpg";
+        img.classList.add("showShow");
+        resultList[0].appendChild(img);
+    }
+}
